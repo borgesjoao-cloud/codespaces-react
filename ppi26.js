92576c7cafcc7c193053550695f60666e6b5bb2c
dobodex7 <borges.joao@escolar.ifrn.edu.br>
2026-05-06 19:19:30 +0000

    Aula 06/05 - Introdução a Javascript

---
+++ ppi26.js
@@ -0,0 +1,31 @@
+console.log("Bm vindo a PPI!");
+
+// Variáveis
+let name = "João";
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; //NÃO PODE MUDAR DE VALOR
+
+console.log(PI);
+
+//Operadores Aritméticos + - * / (modulo=resto)
+
+let x = 5 + 5;
+let y = "5" + 5; //concatenção de texto
+let z = "Hello" + 5;
+
+console.log(x, y, z);
+console.log(typeof x);
+
+//ativar o formartador Pretier  Alt Shift F
+
+//Operadores relacionais
+console.log("5" == 5); // compara tipo OU valor
+console.log("5" === 5); // compara tipo E valor
+
+//Operadores de incremento 
+console.log(x++); 
+console.log(x);
+console.log(++x);
+
