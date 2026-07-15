52fd06de184d6159440d558506fdeb0d0665946f
dobodex7 <borges.joao@escolar.ifrn.edu.br>
2026-07-15 18:31:11 +0000

    NoticeCard e NoticeList 08/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import NoticeList from "./components/NoticeList";
 
 function App() {
+  const notices = [
+    {
+      id: 1,
+      title: "Secitex Macau 2026",
+      category: "Evento",
+      description: "Inscrições abertas para oficinas e palestras.",
+      author: "COINF/MC",
+      date: "01/07/2026",
+      featured: true,
+    },
+    {
+      id: 2,
+      title: "Manutenção do Lab. de Informática 2",
+      category: "Aviso",
+      description: "Indisponível na sexta 03/07/2026",
+      author: "CTI/MC",
+      date: "01/07/2026",
+      featured: false,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          El Bodão <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <Header
+        title={"Programação para Internet"}
+        subtitle={"Curso Técnico Integrado em Informática"}
+      />
+      <NoticeList notices={notices} />
+    </>
   );
 }
 
